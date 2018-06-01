bf80783d54cd0b9377304bf5e19ab03c4f6223d5
TomYoki <toms.yoki@gmail.com>
2018-06-01 03:01:30 +0300

    Forgot to add updated index

--- temp.sh
+++ temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear;
-
+prev=$(<prev.data);
 rating=(["0"]="Ice Cream" ["10"]="Literally a fridge" ["20"]="Holy Potatoes!" ["30"]="Perfect" ["40"]="Good" ["50"]="Poor" ["60"]="Bad" ["70"]="About to burn your house down" ["150"]="You'll need water. Lots of water.");
 
 loadTemp(){
@@ -62,4 +62,6 @@ echo "";
 echo "Highest temp: $act 'C";
 echo "Average temp: $avg 'C";
 echo "Last temp: ${temps[${#temps[@]}]} 'C";
+echo "Temp loss: $(($prev - $avg)) 'C";
 echo "Rating: $rate";
+echo $avg>prev.data;
